3eeeb70224de4c0a0413bc8e78da086fdd8d8996
tamgerine <66058736+tamgerine@users.noreply.github.com>
2022-04-18 13:04:41 -0700

    lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,12 @@
+// A script to allow a user to log in.
+// Takes input from a user and authenticates it for a login.
+// @link   https://tamgerine.github.io/art101/lab4/index.htm
+// @file   This files defines the login class.
+// @author Tamara Basbuyuk.
+// @since  04.18.2022
+//Process login
+	//Enter username and password
+	//Accept user input
+//Authenticate data from database
+	//Confirm and validate “Yes” or “No”
+	//Allow login if data matches
