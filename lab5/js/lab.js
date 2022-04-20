67102bf85d3cdb6d9ea9bcf72c7b23a0bc1884ac
tamgerine <66058736+tamgerine@users.noreply.github.com>
2022-04-20 14:35:29 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Tamara Basbuyuk <tbasbuyu@ucsc.edu>
+ * Created: 20 April 2022
+ * License: Public Domain
+ */
+
+ //Define variables
+ var make = "Toyota";
+ var model = "Prius";
+ var color = "White";
+ var year = 2006;
+
+ //calculate age
+ var age = 2022 - year;
+
+ //print results
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + "<br>");
