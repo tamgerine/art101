f7d1423ab3b3b47d27f26679a698456418a8596c
tamgerine <66058736+tamgerine@users.noreply.github.com>
2022-05-02 19:56:17 -0700

    lab 8

---
+++ lab8/js/lab.js
@@ -0,0 +1,29 @@
+/*
+*Author: Tamara Basbuyuk
+*Created: 05.01.2022
+*License: Public Domain
+*Program written to operate on a number array
+*/
+function square (x){
+  return x*x;
+}
+
+ //test function
+console.log("What is the square of 1", square(1));
+console.log("What is the square of 2", square(2));
+
+array = [100, 81, 4, 16, 42, 144, 10001];
+console.log("My array", array);
+
+var result = array.map(square);
+//should return the squares
+console.log("Square the array", result);
+
+var result = array.map(function(x) {
+  return x ** 0.5;
+});
+//should return the squareroot of the array
+console.log("squareroot of array", result);
+
+var mapResults = "Original array:" + array + ",</br> Square of array:" + array.map(square) + ",</br> Squareroot of array:" + result;
+console.log(mapResults);
